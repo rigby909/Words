18c5a85e2ae523e46642cc06f534513bc1e1fd1f
Grishchenko <rigby909@gmail.com>
2016-03-24 19:45:56 +0600

    files are implemented

--- razm.js
+++ razm.js
@@ -1,6 +1,8 @@
 $(function() {
+	var resultsCounter = 0;
+	var resultsPool = [];
 	$('#start-button').on('click', function() {
-		var letters = $('#text-content').val().split('');
+		var letters = $('#text-content').val().toUpperCase().split('');
 		
 		for (var i=0; i<letters.length; i++) {
 			$('<div></div>')
@@ -22,4 +24,35 @@ $(function() {
 		$(this).addClass('letter-used');
 	});
 	
+	$('#clear-button').on('click', function() {
+		$('#word-content').text('');
+		$('.letter-used').removeClass('letter-used');
+	});
+	
+	$('#check-button').on('click', function() {
+		var word = $('#word-content').text();
+		
+		if (resultsPool.indexOf(word)!=-1) {
+			return;
+		}
+		
+		$.ajax({
+			url: "https://dictionary.yandex.net/api/v1/dicservice.json/lookup",
+			dataType:"jsonp",
+			data: {
+				key: "dict.1.1.20160323T054829Z.7b0911812b321fc8.778ab3624988f7e7b69e55dfe04038d1adefce30",
+				text: word,
+				lang: "ru-ru"
+			}
+		}).then(function(data) {
+			if (data.def && data.def.length> 0) {
+				$('#word-content').text('');
+				$('.letter-used').removeClass('letter-used');
+				resultsCounter++;
+				$('<div></div>').addClass('resutl-item')
+				.html(resultsCounter + ". " + word + " - " + data.def[0].tr[0].text).appendTo('#result-container');
+				resultsPool.push(word);
+			}
+		});
+	});
 });
